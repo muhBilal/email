b649bd575ff07dec727434df273a8fb3bb429ae9
muhBilal <mb336450@gmail.com>
2025-02-14 00:20:28 +0700

    fix middleware

--- app/Http/Controllers/AuthenController.php
+++ app/Http/Controllers/AuthenController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 
@@ -17,20 +18,14 @@ class AuthenController extends Controller
     public function CheckUser(Request $request)
     {
         $request->validate([
-            'email' => 'required|email:users',
+            'email' => 'required|email',
             'password' => 'required|min:8|max:12'
         ]);
 
-        $user = User::where('email', '=', $request->email)->first();
-        if ($user) {
-            if (Hash::check($request->password, $user->password)) {
-                $request->session()->put('loginId', $user->id);
-                return redirect()->route('home');
-            } else {
-                return back()->with('fail', 'Password not match!');
-            }
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return redirect()->route('home')->with('success', 'Login berhasil!');
         } else {
-            return back()->with('fail', 'This email is not register.');
+            return back()->with('fail', 'Email atau password salah.');
         }
     }
 }
