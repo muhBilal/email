1ef96bf1843732dbf7d1c1fa76c04554d5b32eae
SyahrialZky <syahrialrizky04@gmail.com>
2025-02-24 18:54:11 +0700

    Template preview

--- resources/views/pages/email-template/index.blade.php
+++ resources/views/pages/email-template/index.blade.php
@@ -29,60 +29,7 @@
                 aria-expanded="false" aria-controls="modal-create" data-hs-overlay="#modal-create">Create</button>
         </div>
 
-        <div id="modal-create"
-            class="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
-            role="dialog" tabindex="-1" aria-labelledby="modal-create-label">
-            <div
-                class="hs-overlay-animation-target hs-overlay-open:scale-100 hs-overlay-open:opacity-100 scale-95 opacity-0 ease-in-out transition-all duration-200 sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)] flex items-center">
-                <div class="w-full flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto">
-                    <div class="flex justify-between items-center py-3 px-4 border-b">
-                        <h3 id="modal-create-label" class="font-bold text-gray-800">
-                            Tambah Template
-                        </h3>
-                        <button type="button"
-                            class="size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none"
-                            aria-label="Close" data-hs-overlay="#modal-create">
-                            <span class="sr-only">Close</span>
-                            <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M18 6 6 18"></path>
-                                <path d="m6 6 12 12"></path>
-                            </svg>
-                        </button>
-                    </div>
-                    <div class="p-4 overflow-y-auto">
-                        <form action="{{ route('email-template.store') }}" method="POST">
-                            @csrf
-                            <div class="space-y-3">
-                                <label for="name" class="block text-sm font-medium mb-2">Nama</label>
-                                <input id="name" name="name" type="text"
-                                    class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                                    required>
-                            </div>
-                            <div class="space-y-3 mt-5">
-                                <label for="content" class="block text-sm font-medium mb-2">Teks HTML</label>
-                                <textarea id="content" name="content" rows="10"
-                                    class="w-full text-sm font-mono bg-gray-900 text-green-400 border border-gray-700 rounded-md focus:outline-none"
-                                    placeholder="// Tulis HTML disini" required></textarea>
-                            </div>
-                            <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t">
-                                <button type="button"
-                                    class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
-                                    data-hs-overlay="#modal-create">
-                                    Close
-                                </button>
-                                <button type="submit" id="saveTemplate"
-                                    class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
-                                    Save changes
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-
-                </div>
-            </div>
-        </div>
+        
     </div>
     <div class="overflow-x-auto bg-white shadow-md rounded-lg p-4">
         <table id="emailTable" class="min-w-full overflow-hidden divide-y divide-gray-200 rounded-t-lg">
@@ -97,9 +44,65 @@
         </table>
     </div>
 
+        {{-- Modal Create  --}}
+        <div id="modal-create"
+        class="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
+        role="dialog" tabindex="-1" aria-labelledby="modal-create-label">
+        <div
+            class="hs-overlay-animation-target hs-overlay-open:scale-100 hs-overlay-open:opacity-100 scale-95 opacity-0 ease-in-out transition-all duration-200 sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)] flex items-center">
+            <div class="w-full flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto">
+                <div class="flex justify-between items-center py-3 px-4 border-b">
+                    <h3 id="modal-create-label" class="font-bold text-gray-800">
+                        Tambah Template
+                    </h3>
+                    <button type="button"
+                        class="size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none"
+                        aria-label="Close" data-hs-overlay="#modal-create">
+                        <span class="sr-only">Close</span>
+                        <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M18 6 6 18"></path>
+                            <path d="m6 6 12 12"></path>
+                        </svg>
+                    </button>
+                </div>
+                <div class="p-4 overflow-y-auto">
+                    <form action="{{ route('email-template.store') }}" method="POST">
+                        @csrf
+                        <div class="space-y-3">
+                            <label for="name" class="block text-sm font-medium mb-2">Nama</label>
+                            <input id="name" name="name" type="text"
+                                class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                                required>
+                        </div>
+                        <div class="space-y-3 mt-5">
+                            <label for="content" class="block text-sm font-medium mb-2">Teks HTML</label>
+                            <textarea id="content" name="content" rows="10"
+                                class="w-full text-sm font-mono bg-gray-900 text-green-400 border border-gray-700 rounded-md focus:outline-none"
+                                placeholder="// Tulis HTML disini" required></textarea>
+                        </div>
+                        <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t">
+                            <button type="button"
+                                class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+                                data-hs-overlay="#modal-create">
+                                Close
+                            </button>
+                            <button type="submit" id="saveTemplate"
+                                class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+                                Save changes
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 
     {{-- update modal --}}
-    <button class="bg-orange-500 text-white px-4 py-2 rounded-lg hover:bg-orange-700" aria-haspopup="dialog"
+    <button class="bg-orange-500 hidden text-white px-4 py-2 rounded-lg hover:bg-orange-700" aria-haspopup="dialog"
         aria-expanded="false" aria-controls="modal-update" data-hs-overlay="#modal-update" id="update">update</button>
     <div id="modal-update"
         class="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
@@ -156,6 +159,7 @@
         </div>
     </div>
 
+
 @endsection
 
 @push('js')
@@ -185,7 +189,10 @@
                         name: 'content',
                         className: 'whitespace-nowrap px-6 py-4 border-b border-gray-200',
                         render: function(data) {
-                            return data.length > 20 ? data.substr(0, 20) + '...' : data;
+                            const encodedData = encodeURIComponent(data);
+                            return `
+                                <iframe class="w-full h-48 border rounded" srcdoc="${data.replace(/"/g, '&quot;')}"></iframe>
+                            `;
                         }
                     },
                     {
@@ -302,5 +309,6 @@
                 }
             });
         }
+
     </script>
 @endpush
